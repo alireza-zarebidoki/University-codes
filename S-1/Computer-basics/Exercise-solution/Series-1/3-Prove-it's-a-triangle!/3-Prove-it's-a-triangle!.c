20c27f4bb76785e807f32e1ead357b95332a5a05
Alireza ZareBidoki <alireza.zarebidoki.1111@gmail.com>
2025-12-09 22:54:19 +0330

    Create 3-Prove-it's-a-triangle!.c

---
+++ S-1/Computer-basics/Exercise-solution/Series-1/3-Prove-it's-a-triangle!/3-Prove-it's-a-triangle!.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if (a+b>c && a+c>b && b+c>a)
+    {
+        if (a==b && a==c )
+        {
+            printf("Equilateral");
+        }
+        else if (a==b || a==c || b==c)
+        {
+            printf("Isosceles");
+        }
+        else
+        {
+            printf("Scalene");
+        }
+    }
+    else
+    {
+        printf("Invalid");
+    }
+    return 0;
+}
